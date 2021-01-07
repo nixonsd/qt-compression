a7353701691c8902ccbd298a0f672a4bb4534aa9
Bogdan <bandanovvs@gmail.com>
2021-01-07 21:49:34 +0200

    BinaryData class update

--- include/BinaryData.h
+++ include/BinaryData.h
@@ -10,50 +10,50 @@ class BinaryData
 public:
 	~BinaryData(void);
 	/**
-	 * Adds additional nulls
-	 * @param void
-	 * @return void
-	 */
+	  * Adds additional nulls
+	  * @param void
+	  * @return void
+	  */
 	void AddNulls(void);
 	/**
-	 * Tranforms the string of bits to the array of chars
-	 * @param Array of bits
-	 * @return void
-	 */
+	  * Tranforms the string of bits to the array of chars
+	  * @param Array of bits
+	  * @return void
+	  */
 	void AddBits(QString);
 	/**
-	 * Gets the array of chars
-	 * @param void
-	 * @return Array of chars
-	 */
+	  * Gets the array of chars
+	  * @param void
+	  * @return Array of chars
+	  */
 	QString ToString(void);
 	/**
-	 * Clears the array and memory of chars
-	 * @param void
-	 * @return void
-	 */
+	  * Clears the array and memory of chars
+	  * @param void
+	  * @return void
+	  */
 	void ClearChars(void);
 
 public:
 	BinaryData(BinaryData& other) = delete;
 	void operator=(const BinaryData&) = delete;
 	/**
-	 * Gets the instance of the class
-	 * @param void
-	 * @return The class instance
-	 */
+	  * Gets the instance of the class
+	  * @param void
+	  * @return The class instance
+	  */
 	static BinaryData* GetInstance(void);
 	/**
-	 * Gets the position of the highest bit of the number
-	 * @param Input number
-	 * @return Output position of the highest bit
-	 */
+	  * Gets the position of the highest bit of the number
+	  * @param Input number
+	  * @return Output position of the highest bit
+	  */
 	static quint32 HiBit(quint64 x);
 	/**
-	 * Gets the binary representation of the number
-	 * @param Input number
-	 * @return Output binary representation of the number
-	 */
+	  * Gets the binary representation of the number
+	  * @param Input number
+	  * @return Output binary representation of the number
+	  */
 	static QString ToBinary(quint32 n, size_t l = 0);
 
 
@@ -62,16 +62,16 @@ protected:
 
 public:
 	/**
-	 * Returns the current size of internal data
-	 * @param void
-	 * @return Value of the size of data
-	 */
+	  * Returns the current size of internal data
+	  * @param void
+	  * @return Value of the size of data
+	  */
 	size_t GetSize(void) const;
 	/**
-	 * Number of padding zeros
-	 * @param void
-	 * @return Count of the padding zeros
-	 */
+	  * Number of padding zeros
+	  * @param void
+	  * @return Count of the padding zeros
+	  */
 	size_t GetAddedBits(void) const;
 
 private:
